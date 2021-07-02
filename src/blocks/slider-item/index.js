b5e68c37bd99a02fe09ab4e39acca04d6a919e1d
achchu93 <achchu.zats@gmail.com>
2021-07-02 16:49:14 +0530

    added: slide item settings

--- src/blocks/slider-item/index.js
+++ src/blocks/slider-item/index.js
@@ -7,7 +7,21 @@ import './editor.scss';
 import './style.scss';
 
 const { __ } = wp.i18n;
+const { Fragment } = wp.element;
 const { registerBlockType } = wp.blocks;
+const { RichText, MediaUpload } = wp.editor;
+const { useBlockProps, InspectorControls, MediaPlaceholder } = wp.blockEditor;
+const {
+	ToggleControl,
+	TextControl,
+	PanelBody,
+	PanelRow,
+	ButtonGroup,
+	Button,
+	ColorPicker,
+} = wp.components;
+const { isEmpty, pick, replace, capitalize } = lodash;
+const allowedMediaTypes = ["image", "video"];
 
 /**
  * Register: Guten Slider Item.
@@ -20,14 +34,56 @@ const { registerBlockType } = wp.blocks;
  */
 registerBlockType("gsb/block-guten-slider-item", {
 	title: __("Guten Slider Item"),
-	parent: [ 'gsb/block-guten-slider' ],
+	parent: ["gsb/block-guten-slider"],
 	icon: "slides",
 	category: "common",
-	keywords: [
-		__("Guten Slider Item"),
-		__("gsb"),
-		__("slider"),
-	],
+	keywords: [__("Guten Slider Item"), __("gsb"), __("slider")],
+	attributes: {
+		content: {
+			type: "string",
+			default: "",
+		},
+		link: {
+			type: "string",
+			default: "",
+		},
+		alignment: {
+			type: "string",
+			default: "left",
+		},
+		media: {
+			type: "object",
+			default: {},
+		},
+		target: {
+			type: "string",
+			default: "_blank",
+		},
+		enableButton: {
+			type: "boolean",
+			default: true,
+		},
+		buttonText: {
+			type: "string",
+			default: "Click Me",
+		},
+		buttonType: {
+			type: "string",
+			default: "rounded",
+		},
+		buttonColor: {
+			type: "string",
+			default: "#fff",
+		},
+		buttonBgColor: {
+			type: "string",
+			default: "#007cba",
+		},
+		buttonUrl: {
+			type: "string",
+			default: "",
+		},
+	},
 
 	/**
 	 * The edit function describes the structure of your block in the context of the editor.
@@ -40,10 +96,239 @@ registerBlockType("gsb/block-guten-slider-item", {
 	 * @param {Object} props Props.
 	 * @returns {Mixed} JSX Component.
 	 */
-	edit: (props) => {
+	edit: ({
+		attributes: {
+			content,
+			link,
+			alignment,
+			media,
+			enableButton,
+			buttonText,
+			buttonType,
+			buttonColor,
+			buttonBgColor,
+			target,
+		},
+		setAttributes,
+		clientId,
+	}) => {
+		const blockProps = useBlockProps();
+		const onMediaSelect = (mediaInfo) => {
+			const data = pick(mediaInfo, [
+				"url",
+				"id",
+				"sizes",
+				"type",
+				"width",
+				"height",
+			]);
+			setAttributes({ media: data });
+		};
+
 		return (
-			<div className={props.className}>
-				<p>Slider Item</p>
+			<div {...blockProps}>
+				<InspectorControls key="setting">
+					{!isEmpty(media) && (
+						<Fragment>
+							<PanelBody
+								title={__("Slide Image")}
+								initialOpen={true}
+							>
+								<PanelRow>
+									<MediaUpload
+										title={__("Slide image")}
+										onSelect={onMediaSelect}
+										allowedTypes={allowedMediaTypes}
+										value={media.id}
+										render={({ open }) => (
+											<a onClick={open}>
+												<img src={media.url} />
+											</a>
+										)}
+									/>
+								</PanelRow>
+							</PanelBody>
+							<PanelBody
+								title={__("Slide Button")}
+								initialOpen={false}
+							>
+								<PanelRow>
+									<ToggleControl
+										label={__("Enable Slide Button")}
+										checked={enableButton}
+										onChange={(enableButton) =>
+											setAttributes({ enableButton })
+										}
+										help={__(
+											"Show button at end of slide content"
+										)}
+									/>
+								</PanelRow>
+								{enableButton && (
+									<Fragment>
+										<PanelRow>
+											<TextControl
+												label={__("Button Text")}
+												value={buttonText}
+												onChange={(buttonText) =>
+													setAttributes({ buttonText })
+												}
+												help={__("Slide button text")}
+											/>
+										</PanelRow>
+										<PanelRow>
+											<fieldset>
+												<legend>
+													{__("Button Type")}
+												</legend>
+												<ButtonGroup
+													id={`${clientId}-button-type`}
+													aria-label={__(
+														"Button Type"
+													)}
+												>
+													{[
+														"rounded",
+														"flat",
+														"border",
+													].map((_type) => {
+														return (
+															<Button
+																isSmall
+																isPrimary={
+																	buttonType ===
+																	_type
+																}
+																onClick={() =>
+																	setAttributes(
+																		{
+																			buttonType: _type,
+																		}
+																	)
+																}
+															>
+																{capitalize(_type)}
+															</Button>
+														);
+													})}
+												</ButtonGroup>
+											</fieldset>
+										</PanelRow>
+										<PanelRow>
+											<fieldset>
+												<legend className="blocks-base-control__label">
+													{__("Button Text Color")}
+												</legend>
+												<ColorPicker
+													color={buttonColor}
+													onChangeComplete={(color) =>
+														setAttributes({
+															buttonColor:
+																color.hex,
+														})
+													}
+													disableAlpha
+												/>
+											</fieldset>
+										</PanelRow>
+										<PanelRow>
+											<fieldset>
+												<legend className="blocks-base-control__label">
+													{__("Button Color")}
+												</legend>
+												<ColorPicker
+													color={buttonBgColor}
+													onChangeComplete={(color) =>
+														setAttributes({
+															buttonBgColor:
+																color.hex,
+														})
+													}
+													disableAlpha
+												/>
+											</fieldset>
+										</PanelRow>
+									</Fragment>
+								)}
+							</PanelBody>
+							<PanelBody
+								title={__("Slide URL")}
+								initialOpen={false}
+							>
+								<PanelRow>
+									<TextControl
+										label={__("Slide Url")}
+										value={link}
+										onChange={(link) => setAttributes({ link })}
+										help={__("Set a link for the slide")}
+									/>
+								</PanelRow>
+								<PanelRow>
+									<fieldset>
+										<legend>{__("Slide URL Type")}</legend>
+										<ButtonGroup
+											aria-label={__("Slide URL Type")}
+										>
+											{["_blank", "_self", "_parent", "_top"].map(
+												(_target) => {
+													return (
+														<Button
+															isSmall
+															isPrimary={
+																target ===
+																_target
+															}
+															onClick={() =>
+																setAttributes({
+																	target: _target,
+																})
+															}
+														>
+															{capitalize(
+																replace(
+																	_target,
+																	"_",
+																	""
+																)
+															)}
+														</Button>
+													);
+												}
+											)}
+										</ButtonGroup>
+									</fieldset>
+								</PanelRow>
+							</PanelBody>
+						</Fragment>
+					)}
+				</InspectorControls>
+				{isEmpty(media) ? (
+					<MediaPlaceholder
+						onSelect={onMediaSelect}
+						allowedTypes={["image", "video"]}
+						multiple={false}
+						labels={{ title: "Slider Image" }}
+						addToGallery={true}
+						autoOpenMediaUpload={true}
+					/>
+				) : (
+					<figure className="slide-figure">
+						{media.type === "video" ? (
+							<video src={media.url} />
+						) : (
+							<img src={media.url} />
+						)}
+						<RichText
+							className="slide-content"
+							tagName="div"
+							placeholder={__("Add your slider content here")}
+							onChange={() => {
+								setAttributes({ content });
+							}}
+							value={content}
+						/>
+					</figure>
+				)}
 			</div>
 		);
 	},
--- src/init.php
+++ src/init.php
@@ -40,7 +40,7 @@ function guten_slider_block_cgb_block_assets() { // phpcs:ignore
 	wp_register_script(
 		'guten_slider_block-cgb-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
+		array( 'wp-i18n', 'wp-block-editor', 'wp-blocks', 'wp-components' ), // Dependencies, defined above.
 		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
 		true // Enqueue the script in the footer.
 	);
